2bcf6ec0d07c56c72bfaf2b1d87a6cd81cd5a0f1
Mayank Pratap <mayank7860@gmail.com>
2016-06-03 20:41:02 +0530

    Forgot callback and suffered headache

--- js/twitch.js
+++ js/twitch.js
@@ -1,16 +1,14 @@
 include("jquery");
-var arr = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck","brunofin","habathcx", "RobotCaleb", "noobs2ninjas","comstor404"];
+var arr = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck","brunofin","habathcx", "RobotCaleb", "noobs2ninjas","comster404"];
 var len=arr.length;
-//console.log(len);
+console.log(len);
 
 for(var i=0;i<len;++i){
 
    //console.log(arr[i]);
-   var url="https://api.twitch.tv/kraken/streams/"+arr[i];
-  // console.log(url);
-
+   var url="https://api.twitch.tv/kraken/streams/"+arr[i]+"?callback=?";
+   console.log(url);
    $.getJSON(url,function(json){
-
 
      handleMyJSON(json);
 
@@ -18,23 +16,22 @@ for(var i=0;i<len;++i){
 
    function handleMyJSON(json){
 
-      if(json.status=="404"){
-       // console.log(url);
-        console.log("Account Closed");
-      }
-      else{
+     if(json.stream===null){
 
-         if(json.stream===null){
+         console.log("Offline");
 
-           // console.log(url);
-            console.log("Account Inactive");
-         }
+     }
 
-         else{
-          //  console.log(url);
-            console.log(json.stream.game+":"+json.stream.channel.status);
-         }
+     else if(json.stream===undefined){
 
-       }
+         console.log("Account Closed");
+     }
+
+     else{
+
+         console.log("Online");
+     }
+
    }
+
 }  
