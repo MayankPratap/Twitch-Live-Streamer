58d128ed923a7bbc1d7b2644416a94d4fb5f1f44
Mayank Pratap <mayank7860@gmail.com>
2016-06-05 21:27:57 +0530

    Finally getting all info about channels

--- js/twitch.js
+++ js/twitch.js
@@ -1,61 +1,61 @@
 include("jquery");
-var arr = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck","brunofin","habathcx", "RobotCaleb", "noobs2ninjas","comster404"];
-var len=arr.length;
-console.log(len);
+var channels = ["ESL_SC2","OgamingSC2","cretetion","freecodecamp","storbeck","brunofin","habathcx","RobotCaleb","noobs2ninjas","comster404"];
+var len=channels.length;
+//console.log(len);
 
-for(var i=0;i<len;++i){
+channels.forEach(function(channel){
 
    //console.log(arr[i]);
    function makeUrl(which,val){
-
-     if(which=="streams")
-       return "https://api.twitch.tv/kraken/streams/"+val+"?callback=?";
-
-     else if(which=="users")
-       return "https://api.twitch.tv/kraken/users/"+val+"?callback=?";
-
+      if(which=="streams")
+        return "https://api.twitch.tv/kraken/streams/"+val+"?callback=?";
+      else if(which=="channels")
+        return "https://api.twitch.tv/kraken/channels/"+val+"?callback=?";
    }
 
-   $.getJSON(makeUrl("streams",arr[i]),function(json){
+   var game,status;
 
-     val=handleMyJSON(json);
-
-     $.getJSON(val,function(json){
-
-        console.log(json.logo);
-        console.log(json.display_name);
-        console.log(json.game+" "+json.status);
-
-     });
-
-
-   });
-
-   function handleMyJSON(json){
+   $.getJSON(makeUrl("streams",channel),function(json){
 
      if(json.stream===null){
-
-         console.log("Offline");
-        // console.log(json._links.channel);
-         return json._links.channel;
-
+        game="Offline";
+        status="offline";
      }
 
      else if(json.stream===undefined){
-
-         console.log("Account Closed");
-
+        game="Account Closed";
+        status="offline";
      }
 
      else{
-
-         console.log("Online");
-      //   console.log(json._links.channel);
-         return json._links.channel;
-
-
+        game=json.stream.game;
+        status=json.stream.channel.status;
      }
 
-   }
+     var logo,display_name;
 
-}
+     $.getJSON(makeUrl("channels",channel),function(json){
+
+         if(status=="offline"){
+
+           logo=(game=="Offline")?json.logo:"https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
+           display_name=(game=="Offline")?json.display_name:channel;
+           console.log(logo);
+           console.log(display_name);
+           console.log(game);
+
+         }
+
+         else{
+
+           logo=json.logo;
+           display_name=json.display_name;
+           console.log(logo);
+           console.log(display_name);
+           console.log(game+":"+status);
+
+         }
+
+     });
+  });
+});
