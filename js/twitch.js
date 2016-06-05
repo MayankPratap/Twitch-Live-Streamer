a44aa9b445619e5e0e6c395e5402676553f8ad5e
Mayank Pratap <mayank7860@gmail.com>
2016-06-06 00:36:36 +0530

    Syncing gh-pages with master

--- js/twitch.js
+++ js/twitch.js
@@ -1,4 +1,4 @@
-include("jquery");
+$(document).ready(function(){
 var channels = ["ESL_SC2","OgamingSC2","cretetion","freecodecamp","storbeck","brunofin","habathcx","RobotCaleb","noobs2ninjas","comster404"];
 var len=channels.length;
 //console.log(len);
@@ -40,9 +40,13 @@ channels.forEach(function(channel){
 
            logo=(game=="Offline")?json.logo:"https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
            display_name=(game=="Offline")?json.display_name:channel;
-           console.log(logo);
-           console.log(display_name);
-           console.log(game);
+           var html="";
+           html+="<div class='well channel-offline row'><div class='col-sm-4'><img class='img-responsive logo' src='"+logo+"'></img></div><div class='col-sm-4'><a href='https://www.twitch.tv/"+display_name+"' target='_blank'>"+display_name+"</a></div><div class='col-sm-4'><p><i>"+game+"</i></p></div></div>";
+           //console.log(logo);
+           //console.log(display_name);
+           //console.log(game);
+           console.log(html);
+           $('.container').append(html);
 
          }
 
@@ -50,12 +54,19 @@ channels.forEach(function(channel){
 
            logo=json.logo;
            display_name=json.display_name;
-           console.log(logo);
-           console.log(display_name);
-           console.log(game+":"+status);
+           //console.log(logo);
+           //console.log(display_name);  // Use <a href="https://www.twitchtv.com/freecodecamp">display_name</a>
+           //console.log(game+":"+status);
+
+           var html="";
+           html+="<div class='well channel-online row'><div class='col-sm-4'><img class='img-responsive logo' src='"+logo+"'></img></div><div class='col-sm-4'><a href='https://www.twitch.tv/"+display_name+"' target='_blank'>"+display_name+"</a></div><div class='col-sm-4'><p><i>"+game+":"+status+"</i></p></div></div>";
+           console.log(html);
+           $('.container').append(html);
 
          }
 
-     });
+      });
+    });
   });
+
 });
